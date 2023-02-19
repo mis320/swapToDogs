5e4f4f40aba6503abcd192315de5299d8bd68bbd
mis320 <62322924+mis320@users.noreply.github.com>
2023-02-19 22:30:57 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,65 @@
+let str = `https://binance.nodereal.io	25431635	0.317s		
+Connect Wallet
+https://bsc.rpc.blxrbdn.com	25431635	0.336s		
+Connect Wallet
+https://bsc-dataseed3.ninicoin.io	25431635	0.345s		
+Connect Wallet
+https://bsc-dataseed1.ninicoin.io	25431635	0.353s		
+Connect Wallet
+https://bsc-dataseed.binance.org	25431635	0.357s		
+Connect Wallet
+https://rpc-bsc.bnb48.club	25431635	0.400s		
+Connect Wallet
+https://bsc-mainnet.public.blastapi.io	25431635	0.416s		
+Connect Wallet
+https://bsc-mainnet.rpcfast.com?api_key=S3X5aFCCW9MobqVatVZX93fMtWCzff0MfRj9pvjGKSiX5Nas7hz33HwwlrT5tXRM	25431635	0.459s		
+Connect Wallet
+https://endpoints.omniatech.io/v1/bsc/mainnet/public	25431635	0.533s		
+Connect Wallet
+https://1rpc.io/bnb	25431635	0.893s		
+Connect Wallet
+https://bsc.blockpi.network/v1/rpc/public	25431634	0.141s		
+Connect Wallet
+https://rpc.ankr.com/bsc	25431634	0.159s		
+Connect Wallet
+https://bsc-dataseed4.defibit.io	25431634	0.170s		
+Connect Wallet
+https://bsc-dataseed4.ninicoin.io	25431634	0.172s		
+Connect Wallet
+https://bsc-dataseed2.binance.org	25431634	0.182s		
+Connect Wallet
+https://bsc-dataseed3.binance.org	25431634	0.184s		
+Connect Wallet
+https://bsc-mainnet.nodereal.io/v1/64a9df0874fb4a93b9d0a3849de012d3	25431634	0.186s		
+Connect Wallet
+https://bsc-dataseed1.defibit.io	25431634	0.188s		
+Connect Wallet
+https://bsc-dataseed3.defibit.io	25431634	0.188s		
+Connect Wallet
+https://bsc-dataseed1.binance.org	25431634	0.193s		
+Connect Wallet
+https://bsc-dataseed2.defibit.io	25431634	0.197s		
+Connect Wallet
+https://bsc-dataseed4.binance.org	25431634	0.197s		
+Connect Wallet
+https://bsc-dataseed2.ninicoin.io	25431634	0.292s		
+Connect Wallet
+https://bscrpc.com	25431632	0.184s		
+Connect Wallet
+wss://bsc-ws-node.nariox.org				
+https://nodes.vefinetwork.org/smartchain				
+Connect Wallet
+https://bsc-mainnet.rpcfast.com`
+// let aray = str.split('\n')
+// console.log(aray);
+
+
+console.log(str.match(/[a-zA-z]+:\/\/[^\s]*/ig ));
+
+let path = [1, 2, 3]
+console.log(parseInt(138478 /41947));
+console.log(parseInt(138478 + parseInt(138478 /41947) * 20000));
+
+
+console.log(138478* 0.65 *2);
+
